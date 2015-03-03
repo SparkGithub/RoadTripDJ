b0cbd7a9b82f42969b3ab4d790a144d42a0b4846
Spark <spark@SparkMacBook.local>
2015-03-03 10:58:35 +0200

    Initial Commit

---
+++ RoadTripDJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RoadTripDJ
+//
+//  Created by Spark on 3/3/15.
+//  Copyright (c) 2015 Spark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RoadTripDJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RoadTripDJ
+//
+//  Created by Spark on 3/3/15.
+//  Copyright (c) 2015 Spark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
