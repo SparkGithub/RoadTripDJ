ac570b09d391ed6c9896d074dad47f5bfb894d97
SparkGithub <daniel.nadav@gmail.com>
2015-03-03 13:35:11 +0200

    Adding media player's picker

--- RoadTripDJ/ViewController.m
+++ RoadTripDJ/ViewController.m
@@ -7,21 +7,55 @@
 //
 
 #import "ViewController.h"
+@import MediaPlayer;
 
-@interface ViewController ()
+
+@interface ViewController () <MPMediaPickerControllerDelegate>
+
+@property (nonatomic, strong) MPMediaItemCollection *playlist;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)addMusic:(id)sender {
+    MPMediaPickerController *mediaPicker = [[MPMediaPickerController alloc]initWithMediaTypes: MPMediaTypeMusic];
+    mediaPicker.prompt = @"Add music to your playlist";
+    mediaPicker.showsCloudItems = YES;
+    mediaPicker.delegate = self;
+    mediaPicker.allowsPickingMultipleItems = YES;
+    [self presentViewController:mediaPicker animated:YES completion:nil];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - MPMediaPickerControllerDelegate 
+
+-(void)mediaPicker:(MPMediaPickerController *)mediaPicker didPickMediaItems:(MPMediaItemCollection *)mediaItemCollection {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    if (!self.playlist) {
+        self.playlist = mediaItemCollection;
+    } else {
+        NSMutableArray *items = [NSMutableArray arrayWithCapacity:self.playlist.count + mediaItemCollection.count];
+        [items addObjectsFromArray:self.playlist.items];
+        [items addObjectsFromArray:mediaItemCollection.items];
+        MPMediaItemCollection *collection = [MPMediaItemCollection collectionWithItems:items];
+        self.playlist = collection;
+    }
+
+    int index = 1;
+    for (MPMediaItem *item in self.playlist.items) {
+        NSLog(@"%d) %@ - %@", index++, item.artist, item.title);
+    }
+    
+    [ self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)mediaPickerDidCancel:(MPMediaPickerController *)mediaPicker {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 @end
